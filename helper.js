e4b202ec3f6859d5e896b660feb2452400df0085
JOhn V <johndominicvillalino@outlook.com>
2022-04-10 19:28:41 +0800

    todo complete

--- helper.js
+++ helper.js
@@ -1,27 +1,42 @@
 const dom = document;
-let nameState = ''
+let nameState = "";
+let taskState = {};
 const searchIcon = document.getElementById("searchImg");
 const searchInput = dom.getElementById("searchSpan");
-const searchInputVal = dom.getElementById('searchInput')
+const searchInputVal = dom.getElementById("searchInput");
 const time = dom.getElementById("time");
 const greet = dom.getElementById("greetings");
 const amPmGreet = dom.getElementById("amPm");
 const body = dom.getElementById("body");
 const html = dom.querySelector("html");
-const storage = window.localStorage
-const storageNames = storage.getItem('names')
-const parsedName = JSON.parse(storageNames)
-const registerNameSubmit = dom.getElementById('submitName')
-const nameGreet = dom.getElementById('nameGreet')
-const nameInput = dom.getElementById('nameinput')
-const formSearchInput = dom.getElementById('formSearchInput')
-const linkCardClicks = dom.querySelectorAll('.linkCardClicks')
-const linkNav = dom.getElementById('linkNav')
-const linkCard = dom.getElementById('linkCard')
-const newLink = dom.getElementById('newLink')
-const defaultSelection = dom.getElementById('defaultSelection')
+const storage = window.localStorage;
+const storageNames = storage.getItem("names");
+const parsedName = JSON.parse(storageNames);
+const registerNameSubmit = dom.getElementById("submitName");
+const nameGreet = dom.getElementById("nameGreet");
+const nameInput = dom.getElementById("nameinput");
+const formSearchInput = dom.getElementById("formSearchInput");
+const linkCardClicks = dom.querySelectorAll(".linkCardClicks");
+const linkNav = dom.getElementById("linkNav");
+const linkCard = dom.getElementById("linkCard");
+const newLink = dom.getElementById("newLink");
+const defaultSelection = dom.getElementById("defaultSelection");
+const newLinkcard = dom.querySelector(".newLinkcard");
+const linkButton = dom.getElementById("linkButton");
+const newLInksHere = dom.getElementById("newLInksHere");
+const appsLink = dom.getElementById("appsLink");
+const nameLink = dom.getElementById("nameOfLink");
+const linkToAdd = dom.getElementById("linkToAdd");
+const taskCreate = dom.getElementById("taskCreate");
+const taskCounter = dom.getElementById("taskCounter");
+const todoSection = dom.getElementById("todoSection");
+const selectTaskSched = dom.getElementById("selectTaskSched");
+const innerTaskCardContainer = dom.querySelector('.innerTaskCardContainer')
+const dueDate = dom.getElementById('dueDate')
 
-const fadeInFunc = (el,sec) => {
+const linkState = {};
+
+const fadeInFunc = (el, sec) => {
   let loadCount = 0;
   const load = setInterval(fadeInLoad, sec);
   function fadeInLoad() {
@@ -34,8 +49,23 @@ const fadeInFunc = (el,sec) => {
   }
 };
 
+const shakeFunc = (el, sec) => {
+  let loadCount = 0;
+  const load = setInterval(shake, sec);
+  function shake() {
+    loadCount += 1;
+    if (loadCount % 2 === 0) {
+      el.style.right = "50px";
+    } else {
+      el.style.right = "55px";
+    }
+    if (loadCount == sec) {
+      clearInterval(load);
+    }
+  }
+};
 
-const fadeOutFunc = (el,sec) => {
+const fadeOutFunc = (el, sec) => {
   let loadCount = 1;
   const load = setInterval(fadeInLoad, sec);
   function fadeInLoad() {
@@ -48,74 +78,93 @@ const fadeOutFunc = (el,sec) => {
   }
 };
 
-const slideLeft = (el,sec,from,to) => {
-  if(from >= to){         
-    el.style.visibility = 'hidden';
-    return;  
-}
-else {
-    el.style.marginLeft = from + "px";
-    setTimeout(function(){
-        animateLeft(el, from + 1, to);
-    }, sec) 
-}
-
-}
-
-newLink.addEventListener('click', e => {
-slideLeft(defaultSelection,1000,700,100)
-})
-
-
-
-
 //add user to local storage function
 const registerName = (name) => {
-const storageNamesInsideFunc = storage.getItem('names')
-const parsedNameInsideFunc = JSON.parse(storageNamesInsideFunc)
-const currentNameState = []
+  const storageNamesInsideFunc = storage.getItem("names");
+  const parsedNameInsideFunc = JSON.parse(storageNamesInsideFunc);
+  const currentNameState = [];
 
   let updateTo;
-  if(parsedNameInsideFunc) {
-    parsedName.push(name)
-    updateTo = JSON.stringify(parsedName)
-    localStorage.setItem('names',updateTo)
-    
+  if (parsedNameInsideFunc) {
+    parsedName.push(name);
+    updateTo = JSON.stringify(parsedName);
+    localStorage.setItem("names", updateTo);
   } else {
-    currentNameState.push(name)
-    updateTo = JSON.stringify(currentNameState)
-    localStorage.setItem('names',updateTo)
+    currentNameState.push(name);
+    updateTo = JSON.stringify(currentNameState);
+    localStorage.setItem("names", updateTo);
   }
- 
-}
+};
 
+//storage Upload
+const storageUpload = (holder, uploadLocal) => {
+  let links = storage.getItem(holder);
+  links = JSON.parse(links);
+  if (links) {
+    links.push(uploadLocal);
+    storage.setItem(holder, JSON.stringify(links));
+  } else {
+    storage.setItem(holder, JSON.stringify([uploadLocal]));
+  }
+};
+
+//stoage delete
+const storageDelete = (deleteThis, holder) => {
+  let links = storage.getItem("links");
+  links = JSON.parse(links);
+  links = links.filter((e) => e.nameOfLink !== deleteThis);
+  storage.setItem(holder, JSON.stringify(links));
+};
+const storageDeleteTask = (deleteThis, holder) => {
+  let links = storage.getItem(holder);
+  links = JSON.parse(links);
+  links = links.filter((e) => e.taskName !== deleteThis);
+  storage.setItem(holder, JSON.stringify(links));
+};
+
+// check link on load
+const checkLinkFunc = () => {
+  let linksInStorage = storage.getItem("links");
+  linksInStorage = JSON.parse(linksInStorage);
+  try {
+    linksInStorage.forEach((e) => {
+      const li = dom.createElement("li");
+      const img = dom.createElement("img");
+      li.setAttribute("class", "linkCardClicks flex-reverse newLinkClass");
+      img.setAttribute("linkProp", e.linkToAdd);
+
+      img.setAttribute("class", "width20 imgClickLink");
+      img.setAttribute("src", "./assets/forward.svg");
+      const del = dom.createElement("img");
+      del.setAttribute("class", "deleteLink width20");
+      del.setAttribute("src", "./assets/delete.svg");
+      del.setAttribute("onclick", `deleteLink('${e.nameOfLink}')`);
+
+      li.innerText = e.nameOfLink;
+      li.appendChild(img);
+      li.appendChild(del);
+
+      appsLink.after(li);
+    });
+  } catch (error) {}
+};
 
 const timeFunc = () => {
+  let lastPerson = "";
 
-  let lastPerson = '';
+  const amPmGlobal = new Date().getHours() >= 12 ? "PM" : "AM";
 
-  if(parsedName) {
-    const len = parsedName.length - 1;
-    lastPerson = parsedName[len]
-  } else {
-    lastPerson = nameState
-  }
+  const greetings = `Good ${amPmGlobal === "PM" ? "Evening," : "Morning,"}`;
 
-const amPmGlobal = new Date().getHours() > 12 ? "PM" : "AM";
+  greet.textContent = greetings;
 
-const greetings = `Good ${
-  amPmGlobal === "PM" ? "Evening" : "Morning"
-} ${lastPerson}`;
-
-greet.textContent = greetings;
-
- const amPm = new Date().getHours() > 12 ? "PM" : "AM";
+  const amPm = new Date().getHours() >= 12 ? "PM" : "AM";
   let hour =
     new Date().getHours() % 12 === 0
       ? new Date().getHours()
       : new Date().getHours() % 12;
 
-  if(hour === 0) {
+  if (hour === 12) {
     hour = 12;
   }
 
@@ -126,8 +175,57 @@ greet.textContent = greetings;
 
   time.textContent = date;
   amPmGreet.textContent = amPm;
+};
 
-}
+timeFunc();
 
-timeFunc()
+const backFunc = () => {
+  let slideCount = 0;
+  const slideNow = setInterval(function () {
+    slideCount += 20;
+    defaultSelection.style.opacity = 1;
+    defaultSelection.style.display = "flex";
+    defaultSelection.style.marginLeft = `${slideCount}px`;
+    newLinkcard.style.marginLeft = `${slideCount}px`;
+    newLinkcard.style.opacity = slideCount * -1;
+    if (slideCount === 260) {
+      clearInterval(slideNow);
+      defaultSelection.style.marginLeft = 0;
+      newLinkcard.style.display = "none";
+    }
+  }, 10);
+  linkCard.style.width = "260px";
+};
+
+// check task func
+
+const checkTask = () => {
+  let tasks = storage.getItem("tasks");
+  let taskData = JSON.parse(tasks);
+  tasks = JSON.parse(tasks);
+  try {
+    tasks = tasks.length;
+    if (tasks < 1) {
+      taskCounter.style.background = "transparent";
+    } else {
+      taskCounter.style.background = "red";
+      taskData.forEach((e) => {
+      
+        const li = dom.createElement('li')
+        li.textContent = e.taskName
+        const input = dom.createElement('input')
+        li.setAttribute('class','todoTasks')
+        input.setAttribute('id',e.taskName)
+        input.setAttribute('onchange',`taskTodoChange('${e.taskName}')`)
+        input.setAttribute('type','checkbox')
+        li.appendChild(input);
+        innerTaskCardContainer.appendChild(li)
+
+      });
+    }
+  } catch (error) {}
+
+  taskCounter.textContent = tasks == 0 ? '' : tasks;
+};
+
 
--- script.js
+++ script.js
@@ -1,35 +1,64 @@
-//lastperson
-let lastPerson = "";
-if (parsedName) {
-  const len = parsedName.length - 1;
-  lastPerson = parsedName[len];
-}
-
-const greetings = `Good ${amPm === "PM" ? "Evening" : "Morning"}`;
-greetings.textContent = lastPerson;
-
 dom.addEventListener("DOMContentLoaded", (e) => {
   //fade in document
   fadeInFunc(html, 5);
   //time
   setInterval(timeFunc, 1000);
+
+  //load links
+  checkLinkFunc();
+
+  //add on click to all new links
+  dom.querySelectorAll(".imgClickLink").forEach((e) => {
+    e.addEventListener("click", (el) => {
+      const link = el.target.getAttribute("linkprop");
+      if (link.includes("https://")) {
+        window.open(`https://${link}`, "_blank");
+      } else if (!link.includes("https://") && link.includes(".com")) {
+        window.open(`https://${link}`, "_blank");
+      } else {
+        window.open(`https://google.com/search?q=${link}`, "_blank");
+      }
+    });
+  });
+
+  //load name if exist and lock
+  if (storage.getItem("name")) {
+    console.log(storage.getItem("name"));
+    nameGreet.value = storage.getItem("name");
+    nameGreet.readOnly = true;
+  }
+
+  //load check task if exist
+  checkTask();
+
+  //change handle to update task state
+  const taskClass = dom.querySelectorAll(".taskClass");
+  taskClass.forEach((el) => {
+    el.addEventListener("change", (data) => {
+      const { name, value } = data.target;
+      taskState[name] = value;
+    });
+  });
+
+  //default task time
+  let d = new Date();
+  const dateForm = `${d.getFullYear()}-0${d.getMonth() + 1}-${d.getDate()}`;
+
+  dueDate.value = dateForm;
 });
 
-
-
 //search input fade
-searchIcon.addEventListener('mouseover', e => {
-  searchInput.style.opacity = .5;
-})
+searchIcon.addEventListener("mouseover", (e) => {
+  searchInput.style.opacity = 0.5;
+});
 
-searchInput.addEventListener('focusin', e => {
+searchInput.addEventListener("focusin", (e) => {
   searchInput.style.opacity = 1;
-})
+});
 
-searchInput.addEventListener('focusout', e => {
- fadeOutFunc(searchInput,5)
-})
-
+searchInput.addEventListener("focusout", (e) => {
+  fadeOutFunc(searchInput, 5);
+});
 
 //name input handler
 nameInput.addEventListener("change", (e) => {
@@ -42,40 +71,313 @@ registerNameSubmit.addEventListener("click", (e) => {
   registerName(nameState);
 });
 
-
-//search google 
+//search google
 formSearchInput.addEventListener("submit", (e) => {
   e.preventDefault();
   const searchThis = searchInputVal.value;
   fadeOutFunc(searchInput, 5);
   if (searchThis.length < 1) {
-    window.open(`https://google.com`)
+    window.open(`https://google.com`);
   } else {
     window.open(`https://google.com/search?q=${searchThis}`);
   }
   searchInputVal.value = "";
 });
 
-let cardFadeCount = 0
-linkNav.addEventListener('click',() => {
-  cardFadeCount+=1;
-  if(cardFadeCount % 2 === 0) {
-fadeOutFunc(linkCard,5)
-  }else {
-  fadeInFunc(linkCard,5)
-}
-})
-
-
+let cardFadeCount = 0;
+linkNav.addEventListener("click", () => {
+  cardFadeCount += 1;
+  if (cardFadeCount % 2 === 0) {
+    fadeOutFunc(linkCard, 5);
+    linkCard.style.zIndex = -20;
+  } else {
+    fadeInFunc(linkCard, 5);
+    linkCard.style.zIndex = 1;
+  }
+});
 
 //link card actions
-linkCardClicks.forEach(e => {
-  e.addEventListener('click', el => {
-    const {id} = el.target
-    if(id === 'chromeLink') {
-      window.open('app.html?page=apps')
+linkCardClicks.forEach((e) => {
+  e.addEventListener("click", (el) => {
+    const { id } = el.target;
+    if (id === "chromeLink") {
+      window.open("app.html?page=apps");
     } else {
-      window.open('app.html')
+      window.open("app.html");
     }
-  })
-})
+  });
+});
+
+//card slide
+newLink.addEventListener("click", (e) => {
+  let slideCount = 0;
+  const slideNow = setInterval(function () {
+    slideCount -= 20;
+    newLinkcard.style.display = "block";
+    defaultSelection.style.marginLeft = `${slideCount}px`;
+    newLinkcard.style.marginLeft = `${slideCount}px`;
+    newLinkcard.style.opacity = 1;
+    if (slideCount === -260) {
+      clearInterval(slideNow);
+      fadeOutFunc(defaultSelection, 5);
+      defaultSelection.style.display = "none";
+      newLinkcard.style.marginLeft = 0;
+      linkCard.style.width = "fit-content";
+    }
+  }, 10);
+});
+
+backCard.addEventListener("click", (e) => {
+  backFunc();
+  nameLink.style.borderColor = "#6e6e6e";
+  linkToAdd.style.borderColor = "#6e6e6e";
+});
+
+formLinkCard.addEventListener("change", (e) => {
+  const { value, id } = e.target;
+  linkState[id] = value;
+});
+
+//add link to card
+linkButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (nameLink.value.length < 1 && linkToAdd.value.length < 1) {
+    nameLink.style.borderColor = "red";
+    linkToAdd.style.borderColor = "red";
+    return;
+  }
+
+  const uploadLocal = linkState;
+  storageUpload("links", uploadLocal);
+  const li = dom.createElement("li");
+  li.setAttribute("class", "linkCardClicks flex-reverse newLinkClass");
+  const img = dom.createElement("img");
+  img.setAttribute("src", "./assets/forward.svg");
+  img.setAttribute("class", "width20 imgClickLink");
+  li.innerText = linkState.nameOfLink;
+
+  img.setAttribute("onclick", `openLink('${linkState.linkToAdd}')`);
+  li.appendChild(img);
+
+  const del = dom.createElement("img");
+  del.setAttribute("class", "deleteLink width20");
+  del.setAttribute("src", "./assets/delete.svg");
+  del.setAttribute("onclick", `deleteLink('${linkState.nameOfLink}')`);
+
+  li.appendChild(del);
+  appsLink.after(li);
+
+  backFunc();
+  nameLink.value = "";
+  linkToAdd.value = "";
+
+  nameLink.style.borderColor = "#6e6e6e";
+  linkToAdd.style.borderColor = "#6e6e6e";
+});
+
+//onclick new link func outside init
+function openLink(el) {
+  if (el.includes("https://")) {
+    window.open(`https://${el}`, "_blank");
+  } else if (!el.includes("https://") && el.includes(".com")) {
+    window.open(`https://${el}`, "_blank");
+  } else {
+    window.open(`https://google.com/search?q=${el}`, "_blank");
+  }
+}
+//delete link
+function deleteLink(el) {
+  console.log(el);
+  dom.querySelectorAll(".newLinkClass").forEach((e) => {
+    if (e.textContent === el) {
+      fadeOutFunc(e, 5);
+      setTimeout(() => {
+        e.style.display = "none";
+      }, 600);
+    }
+  });
+
+  storageDelete(el, "links");
+}
+
+//name change update
+nameGreet.addEventListener("focusout", (e) => {
+  const { value } = e.target;
+  storage.setItem("name", value);
+  nameGreet.readOnly = true;
+});
+nameGreet.addEventListener("change", (e) => {
+  const { value } = e.target;
+  storage.setItem("name", value);
+  nameGreet.readOnly = true;
+});
+
+let clickCountOfGreet = 0;
+nameGreet.addEventListener("click", () => {
+  clickCountOfGreet += 1;
+  if (clickCountOfGreet % 2 === 0) {
+    nameGreet.readOnly = false;
+  }
+
+  setTimeout(function () {
+    clickCountOfGreet = 0;
+  }, 1000);
+});
+
+//task upload
+
+const taskNameInput = dom.getElementById("taskNameInput");
+
+taskCreate.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  if (!taskState.taskName) {
+    taskNameInput.style.borderBottom = "2px solid red";
+    return;
+  }
+
+  taskState.dueDate = dueDate.value;
+
+  storageUpload("tasks", taskState);
+
+  shakeFunc(todoSection, 6);
+
+  taskCounter.style.background = "red";
+
+  let tasks = storage.getItem("tasks");
+  tasks = JSON.parse(tasks);
+  tasks = tasks.length;
+  taskCounter.style.background = "red";
+
+  const li = dom.createElement("li");
+  console.log(taskState);
+  li.textContent = taskState.taskName;
+  li.setAttribute("class", "todoTasks");
+  const input = dom.createElement("input");
+  input.setAttribute("id", taskState.taskName);
+  input.setAttribute("type", "checkbox");
+  input.setAttribute("onchange", `taskTodoChange('${taskState.taskName}')`);
+  li.appendChild(input);
+
+  innerTaskCardContainer.appendChild(li);
+  taskCounter.textContent = tasks;
+  taskNameInput.style.borderBottom = "2px solid rgb(110, 110, 110)";
+  taskNameInput.value = "";
+
+  //trigger change
+  const event = new Event("change");
+  selectTaskSched.dispatchEvent(event);
+});
+
+function taskTodoChange(e) {
+  dom.querySelectorAll(".todoTasks").forEach((el) => {
+    if (el.textContent === e) {
+      el.style.textDecoration = "line-through";
+      fadeOutFunc(el, 5);
+      setTimeout(function () {
+        el.style.display = "none";
+      }, 600);
+    }
+  });
+  storageDeleteTask(e, "tasks");
+  let counter = storage.getItem("tasks");
+  counter = JSON.parse(counter);
+  counter = counter.length;
+
+  if (counter == 0) {
+    taskCounter.textContent = "";
+    taskCounter.style.backgroundColor = "transparent";
+  } else {
+    taskCounter.textContent = counter;
+  }
+}
+
+//fade in add todo card
+const addTaskButton = dom.getElementById("addTaskButton");
+const addTodo = dom.querySelector(".addTodo");
+let addClickButton = 0;
+addTaskButton.addEventListener("click", (e) => {
+  addClickButton += 1;
+  if (addClickButton % 2 === 0) {
+    fadeOutFunc(addTodo, 5);
+    addTodo.style.zIndex = "-20";
+  } else {
+    fadeInFunc(addTodo, 5);
+    addTodo.style.zIndex = "1";
+  }
+});
+
+selectTaskSched.addEventListener("change", (e) => {
+  console.log("test");
+  const filterTo = e.target.value;
+
+  let tasks = storage.getItem("tasks");
+  tasks = JSON.parse(tasks);
+
+  const filtered = tasks.filter((e) => {
+    const t = Date.parse(e.dueDate);
+    const eMonth = new Date(t).getMonth() +1;
+    const eDate = new Date(t).getDate();
+    const eYear = new Date(t).getFullYear();
+
+    const tMonth = new Date().getMonth() +1;
+    const tDate = new Date().getDate();
+    const tYear = new Date().getFullYear();
+
+    if(filterTo == 'all') {
+      return e;
+    } 
+
+    if(filterTo == 'overdue') {
+      if(eMonth < tMonth || (eMonth == tMonth && eDate < tDate) || eYear < tYear) {
+        return e;
+      }
+    }
+
+    if(filterTo == 'tomorrow') {
+      if(eMonth > tMonth || (eMonth == tMonth && eDate > tDate) || eYear > tYear) {
+        return e;
+      }
+    }
+
+    if(filterTo == 'today') {
+      if(eMonth == tMonth &&  eDate == tDate && eYear == tYear) {
+        return e
+      }
+    }
+
+  });
+
+  dom.querySelectorAll(".todoTasks").forEach((e) => {
+    e.style.display = "none";
+  });
+
+  filtered.forEach((e) => {
+    const li = dom.createElement("li");
+    li.textContent = e.taskName;
+    const input = dom.createElement("input");
+    li.setAttribute("class", "todoTasks");
+    input.setAttribute("id", e.taskName);
+    input.setAttribute("onchange", `taskTodoChange('${e.taskName}')`);
+    input.setAttribute("type", "checkbox");
+    li.appendChild(input);
+    innerTaskCardContainer.appendChild(li);
+  });
+});
+
+// fade in todo lower card
+
+const todoSpan = dom.getElementById("todoSpan");
+const todoCard = dom.querySelector(".todoCard");
+let addClickButtonLower = 0;
+todoSpan.addEventListener("click", (e) => {
+  addClickButtonLower += 1;
+  if (addClickButtonLower % 2 === 0) {
+    fadeOutFunc(todoCard, 5);
+    todoCard.style.zIndex = "-20";
+  } else {
+    fadeInFunc(todoCard, 5);
+    todoCard.style.zIndex = "1";
+  }
+});
